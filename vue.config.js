f7de6ce205035550390e173f51691eb2ea2cf22c
Yuhsak Inoue <yuhsak.inoue@gmail.com>
2021-07-20 11:46:09 +0900

    ページタイトルを設定

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  publicPath: process.env.VUE_APP_PUBLIC_PATH || '/'
+  publicPath: process.env.VUE_APP_PUBLIC_PATH || '/',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'ToDoList',
+    },
+  },
 }
